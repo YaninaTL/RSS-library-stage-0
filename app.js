442bb4e83384490b160623e9f0c76de08a3d1ad0
Yanina Tormos Lopez <moontmorency@gmail.com>
2023-11-16 22:00:51 +0100

    validating html

--- app.js
+++ app.js
@@ -46,64 +46,6 @@ window.addEventListener("scroll", function () {
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
-// profile dropdown menu
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const profileButton = document.getElementById("profile-button");
-//   const menu = document.querySelector(".menu");
-
-//   // Replace this with your own logic to check if the user is logged in
-//   const isUserLoggedIn = true; // Change this based on your authentication system
-
-//   if (isUserLoggedIn) {
-//     // User is logged in, modify the menu
-//     const profileLink = document.createElement("li");
-//     profileLink.innerHTML = '<a href="#">My Profile</a>'; // "My Profile" for logged-in users
-//     menu.querySelector("ul").innerHTML = ""; // Clear the existing menu items
-//     menu.querySelector("ul").appendChild(profileLink); // Add the new menu item
-
-//     const logoutLink = document.createElement("li");
-//     logoutLink.innerHTML = '<a href="#">Log Out</a>'; // "Log Out" for logged-in users
-//     menu.querySelector("ul").appendChild(logoutLink); // Add the new menu item
-
-//     // Add more menu items for logged-in users if needed
-//   } else {
-//     // User is not logged in
-//     // Add menu items for logged-out users
-//     const loginLink = document.createElement("li");
-//     loginLink.innerHTML =
-//       '<a href="./login/login.html" target="_blank">Log In</a>';
-//     menu.querySelector("ul").appendChild(loginLink);
-
-//     const registerLink = document.createElement("li");
-//     registerLink.innerHTML =
-//       '<a href="./reg/reg.html" target="_blank">Register</a>';
-//     menu.querySelector("ul").appendChild(registerLink);
-//   }
-
-//   profileButton.addEventListener("click", function (event) {
-//     event.stopPropagation(); // Prevent the click event from reaching the body
-
-//     if (menu.style.visibility === "hidden" || menu.style.visibility === "") {
-//       menu.style.visibility = "visible";
-//       menu.style.opacity = "1";
-//     } else {
-//       menu.style.visibility = "hidden";
-//       menu.style.opacity = "0";
-//     }
-//   });
-
-//   // Close the menu when clicking outside of it
-//   document.body.addEventListener("click", function () {
-//     menu.style.visibility = "hidden";
-//     menu.style.opacity = "0";
-//   });
-
-//   menu.addEventListener("click", function (event) {
-//     event.stopPropagation(); // Prevent the click event from closing the menu
-//   });
-// });
-
 //popup profile
 document.addEventListener("DOMContentLoaded", function () {
   const profileButton = document.getElementById("profile-button");
